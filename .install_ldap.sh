38e93b85f9b1c546fb3325f10a037a45e9088b63
Philip Kuryloski <pair+pkuryloski@pivotallabs.com>
2014-05-13 14:39:17 -0700

    sudo as necessary
    [#68008022] https://www.pivotaltracker.com/story/show/68008022

--- .install_ldap.sh
+++ .install_ldap.sh
@@ -6,8 +6,8 @@ cd `dirname $0`
 
 set -x
 
-apt-get update
-apt-get install slapd ldap-utils
-ldapadd -Y EXTERNAL -H ldapi:/// -f src/main/resources/ldap_db_init.ldif
-ldapadd -x -D cn=admin,dc=test,dc=com -w password -f src/main/resources/ldap_init.ldif
+sudo apt-get update
+sudo apt-get install slapd ldap-utils
+sudo ldapadd -Y EXTERNAL -H ldapi:/// -f src/main/resources/ldap_db_init.ldif
+sudo ldapadd -x -D cn=admin,dc=test,dc=com -w password -f src/main/resources/ldap_init.ldif
 ldapsearch -h localhost -p 389 -D 'cn=admin,ou=Users,dc=test,dc=com' -w 'adminsecret' -b 'dc=test,dc=com' 'objectClass=*'
