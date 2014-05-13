1be4a26b7e58f7b9baa4404aac68fc04c737ca3e
Philip Kuryloski <pair+pkuryloski@pivotallabs.com>
2014-05-13 15:05:40 -0700

    quiet the ldap setup
    [#68008022] https://www.pivotaltracker.com/story/show/68008022

--- .install_ldap.sh
+++ .install_ldap.sh
@@ -6,8 +6,7 @@ cd `dirname $0`
 
 set -x
 
-sudo apt-get update
-sudo apt-get install slapd ldap-utils
+sudo apt-get -qy update
+sudo apt-get -qy install slapd ldap-utils
 sudo ldapadd -Y EXTERNAL -H ldapi:/// -f src/main/resources/ldap_db_init.ldif
 sudo ldapadd -x -D cn=admin,dc=test,dc=com -w password -f src/main/resources/ldap_init.ldif
-ldapsearch -h localhost -p 389 -D 'cn=admin,ou=Users,dc=test,dc=com' -w 'adminsecret' -b 'dc=test,dc=com' 'objectClass=*'
