c3f74b0c93415b98d5e2e74b99f8a4055440aa16
Philip Kuryloski <pair+pkuryloski@pivotallabs.com>
2014-05-13 14:04:25 -0700

    Attempt at setting up a basic ldap
    [#68008022] https://www.pivotaltracker.com/story/show/68008022

---
+++ .install_ldap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd `dirname $0`
+
+set -x
+
+apt-get update
+apt-get install slapd ldap-utils
+ldapadd -Y EXTERNAL -H ldapi:/// -f src/main/resources/ldap_db_init.ldif
+ldapadd -x -D cn=admin,dc=test,dc=com -w password -f src/main/resources/ldap_init.ldif
+ldapsearch -h localhost -p 389 -D 'cn=admin,ou=Users,dc=test,dc=com' -w 'adminsecret' -b 'dc=test,dc=com' 'objectClass=*'
